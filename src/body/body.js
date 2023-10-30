baa42ef821e1072b5ad1643d29186d30adb250ee
xiaoy <1103098607@qq.com>
2023-10-30 19:54:11 +0800

    init

--- src/body/body.js
+++ src/body/body.js
@@ -4,6 +4,29 @@ export default function Body() {
     return (
         <>
             <div>
+                <section></section>
+                <h1>hello</h1>
+            </div>
+        </>
+    )
+}
+
+function LeftSection() {
+    return (
+        <>
+            <div>
+                <LeftSection />
+                <h1>hello</h1>
+            </div>
+        </>
+    )
+}
+
+function RightSection() {
+    return (
+        <>
+            <div>
+                <section></section>
                 <h1>hello</h1>
             </div>
         </>
