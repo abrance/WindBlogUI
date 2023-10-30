25777cffde1924b673da390bb59d99da202fd8fd
xiaoy <1103098607@qq.com>
2023-10-30 17:44:06 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './App.css';
+import Body from "./body/body";
+import NavHeader from "./header";
+
+
+function App() {
+    return (
+        <div className="App">
+            <NavHeader />
+            <Body />
+        </div>
+    );
+}
+
+export default App;
